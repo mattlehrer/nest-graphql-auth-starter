8bf0386cdb293a649ca359004775d52088b5f0bb
Matt Lehrer <lehrerm@gmail.com>
2019-12-13 13:52:49 -0800

    remove non-generic modules

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
-// import { EpisodesModule } from './episodes/episodes.module';
 import { UsersModule } from './users/users.module';
-// import { PostsModule } from './posts/posts.module';
-// import { SavesModule } from './saves/saves.module';
-// import { PodcastsModule } from './podcasts/podcasts.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    // EpisodesModule,
-    UsersModule,
-    // PostsModule,
-    // SavesModule,
-    // PodcastsModule,
-    AuthModule,
-  ],
+  imports: [UsersModule, AuthModule],
   providers: [],
 })
 export class ApiModule {}
